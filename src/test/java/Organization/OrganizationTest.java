acf64b8aac2ce4d119305e03f7f4425ee8bf3812
Ewelina Głodasik <en.glodasik@gmail.com>
2020-09-07 21:10:52 +0200

    added fakers and method

--- src/test/java/Organization/OrganizationTest.java
+++ src/test/java/Organization/OrganizationTest.java
@@ -4,38 +4,43 @@ import base.BaseTest;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.*;
 
 public class OrganizationTest extends BaseTest {
-    private static String organizationId;
+
+    static List<String> orgIds = new ArrayList<String>();
 
     @Test
-    public void createNewOrganization(){
+    public void createFakeOrganization(){
+        String fakeCompany = faker.company().name();
+        int fakeNumber = faker.number().numberBetween(1, 10);
+        String fakeName = faker.name().username().concat(String.valueOf(fakeNumber)).replace(".", "");
+        String fakeWebsite = faker.internet().url().replaceFirst("www", "http://www");
+
         Response response = given()
                 .spec(reqSpec)
-                .queryParam("displayName", "Name of the Organization")
-                .queryParam("website", "https://neworganization.org")
+                .queryParam("displayName", fakeCompany)
+                .queryParam("name", fakeName)
+               .queryParam("website", fakeWebsite)
                 .when()
                 .post(BASE_URL + ORGANIZATION)
                 .then()
-                .statusCode(200)
+                .statusCode(SC_OK)
                 .extract()
                 .response();
         JsonPath json = response.jsonPath();
-        Assertions.assertThat(json.getString("displayName")).isEqualTo("Name of the Organization");
-        Assertions.assertThat(json.getString("website")).isEqualTo("https://neworganization.org").startsWith("https://");
-        organizationId = json.getString("id");
+        Assertions.assertThat(json.getString("displayName")).isEqualTo(fakeCompany);
+        Assertions.assertThat(json.getString("name")).isEqualTo(fakeName);
+       Assertions.assertThat(json.getString("website")).isEqualTo(fakeWebsite);
 
-        given()
-                .spec(reqSpec)
-                .when()
-                .delete(BASE_URL + ORGANIZATION + "/" + organizationId)
-                .then()
-                .statusCode(200);
-
-
+       orgIds.add(json.getString("id"));
     }
 
     @Test
@@ -46,7 +51,7 @@ public class OrganizationTest extends BaseTest {
                 .when()
                 .post(BASE_URL + ORGANIZATION)
                 .then()
-                .statusCode(400)
+                .statusCode(SC_BAD_REQUEST)
                 .extract()
                 .response();
     }
@@ -60,21 +65,90 @@ public class OrganizationTest extends BaseTest {
                 .when()
                 .post(BASE_URL + ORGANIZATION)
                 .then()
-                .statusCode(200)
+                .statusCode(SC_OK)
                 .extract()
                 .response();
 
         JsonPath json = responseShort.jsonPath();
+        //Trello automatically adds missing characters
         Assertions.assertThat(json.getString("name")).containsIgnoringCase("B").hasSizeGreaterThan(2);
-        organizationId = json.getString("id");
-        System.out.println(json.prettyPrint());
+        orgIds.add(json.getString("id"));
+         }
 
-        given()
+    @Test
+    public void createOrganizationWithHTTPS(){
+                Response response = given()
                 .spec(reqSpec)
+                .queryParam("displayName", "Company with Https Website")
+                .queryParam("website", "https://neworganization.org")
                 .when()
-                .delete(BASE_URL + ORGANIZATION + "/" + organizationId)
+                .post(BASE_URL + ORGANIZATION)
                 .then()
-                .statusCode(200);
+                .statusCode(SC_OK)
+                .extract()
+                .response();
+        JsonPath json = response.jsonPath();
+        Assertions.assertThat(json.getString("displayName")).isEqualTo("Company with Https Website");
+        Assertions.assertThat(json.getString("website")).isEqualTo("https://neworganization.org").startsWith("https://");
+                orgIds.add(json.getString("id"));
+    }
+    @Test
+    public void createOrganizationWithHTTP(){
+        Response response = given()
+                .spec(reqSpec)
+                .queryParam("displayName", "Company with Http Website")
+                .queryParam("website", "http://neworganization.org")
+                .when()
+                .post(BASE_URL + ORGANIZATION)
+                .then()
+                .statusCode(SC_OK)
+                .extract()
+                .response();
+        JsonPath json = response.jsonPath();
+        Assertions.assertThat(json.getString("displayName")).isEqualTo("Company with Http Website");
+        Assertions.assertThat(json.getString("website")).isEqualTo("http://neworganization.org").startsWith("http://");
+                orgIds.add(json.getString("id"));
     }
 
+    @Test
+    public void createOrganizationsWithNonUniqueNames() {
+        String fakeName = faker.name().username().concat("6").replace(".", "");
+
+                Response response1 = given()
+                .spec(reqSpec)
+                .queryParam("displayName", "Trello Organization1")
+                .queryParam("name", fakeName)
+                .when()
+                .post(BASE_URL + ORGANIZATION)
+                .then()
+                .statusCode(SC_OK)
+                .extract()
+                .response();
+        JsonPath json = response1.jsonPath();
+        Assertions.assertThat(json.getString("displayName")).isEqualTo("Trello Organization1");
+        Assertions.assertThat(json.getString("name")).isEqualTo(fakeName);
+        orgIds.add(json.getString("id"));
+
+        Response response2 = given()
+                .spec(reqSpec)
+                .queryParam("displayName", "Trello Organization2")
+                .queryParam("name", fakeName)
+                .when()
+                .post(BASE_URL + ORGANIZATION)
+                .then()
+                .statusCode(SC_OK)
+                .extract()
+                .response();
+        JsonPath json2 = response2.jsonPath();
+        Assertions.assertThat(json2.getString("displayName")).isEqualTo("Trello Organization2");
+        Assertions.assertThat(json2.getString("name")).contains(fakeName);
+        orgIds.add(json2.getString("id"));
+           }
+
+           @AfterAll
+           public static void deleteOrganizations() {
+               for (String org : orgIds) {
+                   deleteOrganization(org, reqSpec, BASE_URL, ORGANIZATION);
+               }
+           }
 }
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,13 +1,16 @@
 package base;
 
+import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
+import static io.restassured.RestAssured.given;
+
 
 public class BaseTest {
-    protected static final String KEY;
-    protected static final String TOKEN;
+    protected static final String KEY = "03d14d3d1406ccfa2a667c41411adfd2";
+    protected static final String TOKEN = "9f1bed34090134af6b3a3ebd6863659b2fd54499dee9b298f1ada07ef6986881";
     protected static final String BASE_URL = "https://api.trello.com/1/";
     protected static final  String BOARDS = "boards";
     protected static final String LISTS = "lists";
@@ -17,13 +20,29 @@ public class BaseTest {
     protected static RequestSpecBuilder reqBuilder;
     protected static RequestSpecification reqSpec;
 
+
+    protected static Faker faker;
+
+
     @BeforeAll
     public static void beforeAll(){
         reqBuilder = new RequestSpecBuilder();
         reqBuilder.addQueryParam("key", KEY);
         reqBuilder.addQueryParam("token", TOKEN);
         reqBuilder.setContentType(ContentType.JSON);
-
         reqSpec = reqBuilder.build();
+
+        faker = new Faker();
+
     }
-}
+
+    public static void deleteOrganization(String orgId, RequestSpecification reqSpec, final String BASE_URL, final String ORGANIZATION) {
+        given()
+                .spec(reqSpec)
+                .when()
+                .delete(BASE_URL + ORGANIZATION + "/" + orgId)
+                .then()
+                .statusCode(200);
+    }
+
+   }
